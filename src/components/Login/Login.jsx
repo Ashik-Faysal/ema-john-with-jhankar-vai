89bb4eb09f1dd6de75e6c87061f5cb6c88d53700
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-27 17:31:07 +0600

    added sign Up form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import "./Login.css"
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            This is a login page.
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form>
+        <div className="form-control">
+          <label>Email</label>
+          <input type="email" name="email" placeholder="email" required />
         </div>
-    );
+        <div className="form-control">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="password" required />
+        </div>
+<input className="btn-submit" type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,9 +3,34 @@ import "./Signup.css"
 
 const Signup = () => {
     return (
-        <div>
-            <h1>This is sign UP page</h1>
-        </div>
+      <div className="form-container">
+        <h2 className="form-title">Sign Up</h2>
+        <form>
+          <div className="form-control">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="email" required />
+          </div>
+          <div className="form-control">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="confirm"
+              placeholder="Confirm password"
+              required
+            />
+          </div>
+          <input className="btn-submit" type="submit" value="Sign Up" />
+        </form>
+      </div>
     );
 };
 

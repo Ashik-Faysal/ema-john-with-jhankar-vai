5059c057bd24b16429f7476e7037af52b69e6cf1
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-04-27 22:20:28 +0600

    added toggle button for show password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Login.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
 const Login = () => {
+  const [show, setShow] = useState(false);
+
   const { signIn } = useContext(AuthContext);
 
   const navigate = useNavigate();
-const location = useLocation()
-  console.log(location)
-  
+  const location = useLocation();
+  console.log(location);
+
   const from = location.state?.from?.pathname || "/";
 
   const handleLogin = (event) => {
@@ -22,14 +24,14 @@ const location = useLocation()
 
     signIn(email, password)
       .then((result) => {
-      const loggedUser = result.user;
-      console.log(loggedUser);
+        const loggedUser = result.user;
+        console.log(loggedUser);
         form.reset();
-        navigate(from, {replace:true})
+        navigate(from, { replace: true });
       })
-      .catch(error => {
-      console.log(error)
-    })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="form-container">
@@ -42,11 +44,20 @@ const location = useLocation()
         <div className="form-control">
           <label>Password</label>
           <input
-            type="password"
+            type={show ? "text" : "password"}
             name="password"
             placeholder="password"
             required
           />
+          <p
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            <small>
+              {show ? <span>Hide Password</span> : <span>Show Password</span>}
+            </small>
+          </p>
         </div>
         <input className="btn-submit" type="submit" value="Login" />
       </form>
